c4f04f76ec7e313927f9cb1915b362ff552c00dd
DesarrolloPracticas2TW <desarrollo_practicas2@tactilware.com>
2023-05-08 17:33:09 +0200

    first commit

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,41 @@
+import 'package:get/get.dart';
+
+class Dimensions{
+  //*SCREEN SIZE*//
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+  //* /////////////////////////////// *//
+
+  //*CONTAINER SPECIFICS*//
+  static double pageViewContainer = screenHeight/3.84;
+  static double pageViewTextContainer = screenHeight/7.03;
+  static double pageView = screenHeight/2.64;
+  //* /////////////////////////////// *//
+
+  //*HEIGHT*//
+  static double height10 = screenHeight/84.4;
+  static double height15 = screenHeight/56.27;
+  static double height20 = screenHeight/42.2;  
+  static double height25 = screenHeight/33.76;  
+  static double height30 = screenHeight/28.13;  
+  static double height45 = screenHeight/18.75;  
+  //* /////////////////////////////// *//
+
+  //*WIDTH*//
+  static double width10 = screenWidth/39.0;
+  static double width15 = screenWidth/26.0;
+  static double width20 = screenWidth/19.5;
+  static double width30 = screenWidth/13.0;
+  static double width45 = screenWidth/8.66;
+  //* /////////////////////////////// *//
+
+  //*FONT*//
+  static double font12 = screenHeight/70.33;
+  static double font20 = screenHeight/42.2;
+  //* /////////////////////////////// *//
+
+  //*RADIUS*//
+  static double radius20 = screenHeight/42.2;
+  static double radius30 = screenHeight/28.13;
+  //* /////////////////////////////// *//
+}
